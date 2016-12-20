b993c3710dbf1a2797dbcec1acd8f4ed4c4e1928
Noah Wisch <noah.wisch@gmail.com>
2016-12-20 09:40:25 -0500

    Added notes

---
+++ Notes/app.js
@@ -0,0 +1,12 @@
+/**
+ * Great thing #1: Templating
+ * 
+ * Idea is that we can simplify DOM creation by using patterns.
+ * There are 3rd party libraries we can use to take advantage of patterns.
+ * 
+ * Great thing #2: Gulp
+ */
+
+window.addEventListener('load', function () {
+
+});
